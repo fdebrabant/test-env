e04d72fab2a9affe349670c94e79046eb69f3e54
f.debrabant <f.debrabant@groupeonepoint.com>
2024-03-19 10:17:37 +0100

    init/test-config

---
+++ index.test.ts
@@ -0,0 +1,10 @@
+import {describe, expect, test} from '@jest/globals';
+import {sum} from "./index";
+
+
+
+describe('sum', () => {
+    test('adds 1 + 2 to equal 3', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+});
